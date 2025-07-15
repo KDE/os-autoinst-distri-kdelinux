5db137bfe434f103372dd2b2d509c0f9c0c07b61
Kangwei Zhu <anicaazhu@gmail.com>
2025-07-15 19:11:32 -0400

    fix missing import os, sys in system_tray testing

--- tests/kdelinux/panel/system_tray.py
+++ tests/kdelinux/panel/system_tray.py
@@ -1,4 +1,5 @@
 from testapi import *
+import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
 from lib.sessions.plasma_desktop import PlasmaDesktopSession
 
