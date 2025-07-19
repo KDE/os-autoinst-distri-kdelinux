3852ee9f7e51af5328ebb32bdf0bda215206a884
Kangwei Zhu <anicaazhu@gmail.com>
2025-07-18 20:48:55 -0400

    fix __init__.py in strategy

--- lib/strategies/__init__.py
+++ lib/strategies/__init__.py
@@ -1,19 +1,8 @@
 from lib.strategies.kickoff import KickoffOpenStrategy
 from lib.strategies.konsole import KonsoleOpenStrategy
 from lib.strategies.krunner import KRunnerOpenStrategy
-from lib.strategies.tty import TTYOpenStrategyfrom testapi import *
-
-from lib.sessions.syscore.tty import TTYSession
-from lib.strategies.base import OpenStrategy
-
-
-class TTYOpenStrategy(OpenStrategy):
-    def open_app(self, app_name, tty_number=3):
-        (
-            TTYSession
-                .ensure_active(tty_number=3)
-                .type_and_submit(app_name)
-        )
+from lib.strategies.tty import TTYOpenStrategy
+from testapi import *
 
 OPEN_STRATEGIES = {
     'krunner' : KRunnerOpenStrategy(),
