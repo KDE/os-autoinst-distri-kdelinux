f1eb27bbd0a656ecbc55eb4c78376792726b52bb
Kangwei Zhu <anicaazhu@gmail.com>
2025-07-23 20:11:04 -0400

    Save the openqa generated testresults as artifacts

--- utils/configure_git.sh
+++ utils/configure_git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# remember to replace it with your info.
 git config --global --add safe.directory /var/lib/openqa/share/tests/kde-linux
 git config --global user.email "anicaazhu@gmail.com"
 git config --global user.name "Anicaa Zhu"
