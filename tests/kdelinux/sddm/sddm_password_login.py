38a3fd2e687a2aa39d5edf1a699507fa21398534
Bhushan Shah <bhushan.shah@machinesoul.in>
2026-05-08 07:35:19 +0000

    tests: renable sddm_password_login test
    Most of code was commented out, re-enable it to allow to login after user
    is setup with plasma-setup.

--- tests/kdelinux/sddm/sddm_password_login.py
+++ tests/kdelinux/sddm/sddm_password_login.py
@@ -1,19 +1,19 @@
-#from testapi import *
-#from lib.openqa.sessions.syscore.plasma_desktop import PlasmaDesktopSession
-#from lib.openqa.utils import type_and_submit
+from testapi import *
+from lib.openqa.sessions.syscore.plasma_desktop import PlasmaDesktopSession
+from lib import user_manager
 
+def run(self):
+   # check whether booted into desktop screen
+    assert_and_click(
+        'sddm_password_input',
+        'timeout', 60
+    )
+    type_string(user_manager.installed().pw, "max_interval", 250)
+    send_key('ret')
 
-#def run(self):
-    # check whether booted into desktop screen
-#    assert_and_click(
-#        'sddm_password_input',
-#        'timeout', 60
-#    )
-#    type_and_submit('1122334455')
-
-    # DO_INSTALL = 0 indicates the full system boot up for the first time after installing from live.
-    # Check if welcome exists
-#    do_install = get_var('DO_INSTALL')
-#    if do_install == '0':
-#        assert_screen('kdelinux_desktop_welcome', timeout=60)
-#    PlasmaDesktopSession.ensure_active()
+   # DO_INSTALL = 0 indicates the full system boot up for the first time after installing from live.
+   # Check if welcome exists
+    do_install = get_var('DO_INSTALL')
+    if do_install == '0':
+        assert_screen('kdelinux_desktop_welcome', timeout=60)
+    PlasmaDesktopSession.ensure_active()
