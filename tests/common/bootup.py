9401c5c4548481ce9dcb18619e7b54bfedc0b1fc
Bhushan Shah <bhushan.shah@machinesoul.in>
2026-05-15 15:35:05 +0000

    tests: uefi screen shows up only on live
    If we have system installed, there is only one entry and we will not see
    UEFI screen

--- tests/common/bootup.py
+++ tests/common/bootup.py
@@ -3,8 +3,9 @@ from testapi import *
 def run(self):
     power('on')
     DO_INSTALL = get_var("DO_INSTALL")
-    assert_screen('uefi_screen', 'timeout', 30)
-    send_key('ret')
+    if DO_INSTALL == '1':
+        assert_screen('uefi_screen', 'timeout', 30)
+        send_key('ret')
     assert_screen('booting_screen', 'timeout', 30)
     if DO_INSTALL == '1':
         assert_screen('welcome_desktop_screen', 'timeout', 60)
