16087eb691d8c4dbc6c1f3f334143ed319a3fc1b
Thomas Duckworth <tduck@filotimoproject.org>
2026-06-20 21:45:40 +1000

    Ensure mode is correct

--- lib/paths.py
+++ lib/paths.py
@@ -1,1 +1,9 @@
-../extensions/openqa/usr/lib/kde-linux-openqa/lib/paths.py
+# SPDX-License-Identifier: LGPL-2.0-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+# SPDX-FileCopyrightText: 2026 Thomas Duckworth <tduck@filotimoproject.org>
+
+# Paths on the SUT
+
+VAR_DIR      = '/var/lib/kde-linux-openqa'
+VENV_DIR     = f'{VAR_DIR}/venv'
+LIB_DIR      = '/usr/lib/kde-linux-openqa'
+RESULTS_DIR  = '/var/log/kde-linux-openqa'
