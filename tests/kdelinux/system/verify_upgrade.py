66d97ea49f65b72747179a51db8d33a1e958b6b3
Bhushan Shah <bhushan.shah@machinesoul.in>
2026-07-24 16:31:29 +0530

    tests: verify that after upgrade old build is bootable
    - In verify_upgrade set the BOOT_PREVIOUS var, which makes bootup.py
      select the old build
    - If BOOT_PREVIOUS is set, verify_upgrade test case verifies that booted
      build is indeed old build

--- tests/kdelinux/system/verify_upgrade.py
+++ tests/kdelinux/system/verify_upgrade.py
@@ -6,6 +6,19 @@ from lib.openqa.cli_session import session
 from lib import user_manager
 
 def run(self):
-    system_build = session.run('source /etc/os-release; echo $IMAGE_VERSION')
+
+    # This is booted image version
+    system_build = session.run('source /usr/lib/os-release; echo $IMAGE_VERSION')
+    # This is the BUILD number set by openQA
     test_build = get_var('BUILD')
-    assert system_build.strip() == test_build.strip(), f"Both IMAGE_VERSION={system_build} and BUILD={test_build} do not match, upgrade possibly failed"
+
+    # If we are booting in previous boot, BUILD and IMAGE_VERSION should not match
+    boot_prev = get_var('BOOT_PREVIOUS', '0')
+    if boot_prev == '0':
+        assert system_build.strip() == test_build.strip(), f"Both IMAGE_VERSION={system_build} and BUILD={test_build} do not match, upgrade possibly failed"
+    else:
+        assert system_build.strip() != test_build.strip(), f"Both IMAGE_VERSION={system_build} and BUILD={test_build} match, we could not reboot in old build"
+
+    # After we verify that we upgraded to new system, we will boot to previous
+    # image to make sure that old image is still bootable
+    set_var('BOOT_PREVIOUS', '1')
